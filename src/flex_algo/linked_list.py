3e631272a199cb95b7ec4e97a3a7ab32f33eaf43
boyuan <boyuan954@gmail.com>
2023-01-21 19:09:47 +1100

    is cyclic

--- src/flex_algo/linked_list.py
+++ src/flex_algo/linked_list.py
@@ -67,6 +67,33 @@ class LinkedList:
         pre_start.next = pre
         start_node.next = curr
 
+    def is_cyclic(self):
+        tortoise = self.head
+        hare = self.head
+
+        while tortoise is not None and hare is not None:
+            tortoise = tortoise.next
+            hare = hare.next
+
+            if hare is not None:
+                hare = hare.next
+
+            if tortoise == hare:
+                break
+
+        if tortoise is None or hare is None:
+            return None
+
+        p1 = self.head
+        # meeting point
+        p2 = tortoise
+
+        while p1 != p2:
+            p1 = p1.next
+            p2 = p2.next
+
+        return p1
+
 
 class Node:
     def __init__(self, data):
@@ -77,10 +104,48 @@ class Node:
         return str(self.data)
 
 
+def is_cyclic(head):
+    tortoise = head
+    hare = head
+
+    while tortoise is not None and hare is not None:
+        tortoise = tortoise.next
+        hare = hare.next
+
+        if hare is not None:
+            hare = hare.next
+
+        if tortoise == hare:
+            break
+
+    if tortoise is None or hare is None:
+        return None
+
+    p1 = head
+    # meeting point
+    p2 = tortoise
+
+    while p1 != p2:
+        p1 = p1.next
+        p2 = p2.next
+
+    return p1
+
+
 if __name__ == '__main__':
     print('=============testing Linked List==========')
-    node_a = Node('a')
+    node_a = Node(1)
     print(node_a)
+    node_b = Node(2)
+    node_a.next = node_b
+    node_c = Node(3)
+    node_b.next = node_c
+    node_d = Node(4)
+    node_c.next = node_d
+    node_d.next = node_b
+
+    cyclic_node = is_cyclic(node_a)
+    print(cyclic_node)
 
     llist = LinkedList()
 
@@ -91,6 +156,9 @@ if __name__ == '__main__':
     llist.push_back(5)
     print(llist)
 
+    cyclic = llist.is_cyclic()
+    print(cyclic)
+
     llist.reverse_between(2, 4)
     print(llist)
 
@@ -98,5 +166,4 @@ if __name__ == '__main__':
     llist2.push_back(5)
     llist2.reverse_between(1, 1)
     print(llist2)
-
 
